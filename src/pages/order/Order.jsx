6d0f0f2fb5f45d090d5b9691e75800264de34dc8
shalini_10080 <shalini7217625537@gmail.com>
2025-01-08 14:35:58 +0530

    some changes

--- src/pages/order/Order.jsx
+++ src/pages/order/Order.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Order() {
   return (
-    <div>Order</div>
+    <div className="flex justify-center items-center h-screen bg-gray-800">
+    <h1 className="flex items-center text-center text-white text-xl">
+      <span>😟</span>
+      <span className="ml-2">Oops!! You haven't ordered anything</span>
+    </h1>
+  </div>
+  
+
   )
 }
 
