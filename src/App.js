7e0b42a33fda22d06b591d76f90a0cb401f5d19d
Shalini <122449558+shalini1008@users.noreply.github.com>
2025-02-21 15:33:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,9 +27,10 @@ function App() {
         <Route path="/siginnup" element={<Siginup/>}/>
         <Route path="/*" element={<Nopage/>}/>
         <Route path="/productinfo/:id" element={<ProductInfo/>}/>
-        <Route path="/Addproduct" element={<AddProduct />}></Route>
-        <Route path="/updateproduct" element={
-          <UpdateProduct/>}></Route>
+        <Route path="/Addproduct" element={<ProtectedRoutesForAdmin><AddProduct /></ProtectedRoutesForAdmin>}></Route>
+        <Route path="/updateproduct" element={<ProtectedRoutesForAdmin>
+          <UpdateProduct/>
+          </ProtectedRoutesForAdmin>}></Route>
       </Routes>
     </Router>
     </MyState>
@@ -44,14 +45,14 @@ export const ProtectedRoutes = ({children}) => {
     return <Navigate to='/login'/>
   }
 }
-// export const ProtectedRoutesForAdmin = ({children}) => {
-//   const admin = JSON.parse(localStorage.getItem('user'))
-//   console.log(admin.user.email)
-//   if(admin.user.email==='shalini7217625537@gmail.com'){
-//     return children
-//   }else{
-//     toast.error('You are not Admin');
-//     return <Navigate to='/login'/>
-//   }
-// }
+export const ProtectedRoutesForAdmin = ({children}) => {
+  const admin = JSON.parse(localStorage.getItem('user'))
+  console.log(admin.user.email)
+  if(admin.user.email==='shalini7217625537@gmail.com'){
+    return children
+  }else{
+    toast.error('You are not Admin');
+    return <Navigate to='/login'/>
+  }
+}
 export default App
